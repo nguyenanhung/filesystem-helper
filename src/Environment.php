8390860faa249fa85e8b93557129a8c205846a9e
nguyenanhung <hungna.dev@gmail.com>
2024-12-02 12:45:29 +0700

    Update CDN

--- src/Environment.php
+++ src/Environment.php
@@ -19,8 +19,8 @@ namespace nguyenanhung\Libraries\Filesystem;
  */
 interface Environment
 {
-    const VERSION = '1.1.0';
-    const LAST_MODIFIED = '2023-08-22';
+    const VERSION = '1.1.1';
+    const LAST_MODIFIED = '2024-12-02';
     const AUTHOR_NAME = 'Hung Nguyen';
     const AUTHOR_EMAIL = 'dev@nguyenanhung.com';
     const AUTHOR_URL = 'https://nguyenanhung.com';
